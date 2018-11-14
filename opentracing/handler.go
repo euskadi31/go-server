58a9b8f695b8ac455b374a65a5e96487c1aa6db8
Axel Etcheverry <axel.etcheverry@getmyfox.com>
2018-11-14 18:05:58 +0100

    fix(opentracing): bad imports

--- opentracing/handler.go
+++ opentracing/handler.go
@@ -7,13 +7,11 @@ package opentracing
 import (
 	"bytes"
 	"net/http"
-	"strconv"
 	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
-	otlog "github.com/opentracing/opentracing-go/log"
 	"github.com/rs/zerolog/log"
 )
 
