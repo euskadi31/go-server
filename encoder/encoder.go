dae2903742ff5bfd30b62806a4d2fb3c49a8ec62
Axel Etcheverry <axel.etcheverry@getmyfox.com>
2018-11-27 14:01:08 +0100

    feat(encoder): added base of response encoder

---
+++ encoder/encoder.go
@@ -0,0 +1,15 @@
+// Copyright 2017 Axel Etcheverry. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package encoder
+
+import (
+	"net/http"
+)
+
+// Encoder provider
+type Encoder interface {
+	MimeType() string
+	Encode(w http.ResponseWriter, data interface{}) error
+}
