f45837cf785b7547b51c9fa2b6e19bd55b7b0de2
Axel Etcheverry <axel@etcheverry.biz>
2018-12-04 23:27:57 +0100

    test(opentracing): added more tests

--- opentracing/request_test.go
+++ opentracing/request_test.go
@@ -9,6 +9,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/justinas/alice"
 	"github.com/opentracing/opentracing-go/mocktracer"
 )
@@ -21,8 +23,33 @@ func TestWrapRequest(t *testing.T) {
 
 	middleware := alice.New(Handler(tracer, IgnoreNone)).ThenFunc(func(w http.ResponseWriter, r *http.Request) {
 		subReq := httptest.NewRequest("GET", "http://example.com/foo", nil)
-		subReq = WrapRequest(r.Context(), subReq)
+		subReq2 := WrapRequest(r.Context(), subReq)
+
+		assert.Equal(t, subReq, subReq2)
 	})
 
 	middleware.ServeHTTP(w, req)
+}
+
+func TestWrapRequestWithPort(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	w := httptest.NewRecorder()
+
+	tracer := mocktracer.New()
+
+	middleware := alice.New(Handler(tracer, IgnoreNone)).ThenFunc(func(w http.ResponseWriter, r *http.Request) {
+		subReq := httptest.NewRequest("GET", "http://example.com:8080/foo", nil)
+		subReq2 := WrapRequest(r.Context(), subReq)
+
+		assert.Equal(t, subReq, subReq2)
+	})
+
+	middleware.ServeHTTP(w, req)
+}
+
+func TestWrapRequestWithoutTracer(t *testing.T) {
+	r := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	r2 := WrapRequest(r.Context(), r)
+
+	assert.Equal(t, r, r2)
 }
